6f1f2fb2935e262efe6593907b9f73c20bc579b5
clam119 <christopher.lam119@gmail.com>
2022-10-27 20:25:10 +0100

    Add marketplace, items, basket components and routes.

--- src/App.js
+++ src/App.js
@@ -4,25 +4,33 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
-// import Marketplace from './components/Marketplace';
+import Marketplace from './components/Marketplace';
 import Login from './components/Login';
+import SingleItem from './components/SingleItem';
+import Basket from './components/Basket';
 import { UserContext } from './components/context/UserContext';
-
+import { BasketContext } from './components/context/BasketContext';
 function App() {
   const [username, setUsername] = useState('Not Logged In');
-
+  const [basket, setBasket] = useState([]);
+  const [finalPrice, setFinalPrice] = useState(0);
   return (
   <UserContext.Provider value={{ username, setUsername }}>
+  <BasketContext.Provider value={{ basket, setBasket, finalPrice, setFinalPrice }}>
     <div className="App">
       <BrowserRouter>
           <Header />
             <Routes>
               <Route path = "/" element={ <Home />} />
               <Route path = "/login" element={ <Login />} />
+              <Route path = "/marketplace" element={ <Marketplace />} />
+              <Route path = "/items/:item_id" element={ <SingleItem setFinalPrice={setFinalPrice} />} />
+              <Route path ="/basket" element = {<Basket finalPrice={finalPrice} />} />
             </Routes>
           <Footer/>
      </BrowserRouter>
     </div>
+  </BasketContext.Provider>
   </UserContext.Provider>
   );
 }
