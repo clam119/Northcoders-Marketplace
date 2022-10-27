2ad893409919ba1b5ff83f7c35056b27191f9177
clam119 <christopher.lam119@gmail.com>
2022-10-27 20:30:00 +0100

    Remove Dropdown Cart.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from './context/UserContext';
-
+import DropdownCart from './DropdownCart';
 export default function Header () {
     const { username, setUsername } = useContext(UserContext);
  
@@ -32,6 +32,9 @@ export default function Header () {
                         </li>
                         <li>
                             <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Items</a>
+                        </li>
+                        <li>
+                            <Link to="/basket"> <DropdownCart /> </Link>
                         </li>
                         <li>
                             <Link to= "/login"> <button type="button" className="block py-1 pr-4 pl-3 text-white bg-blue-700 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-700">Login</button> </ Link>
