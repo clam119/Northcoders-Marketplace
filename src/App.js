792c87d17b31f68fc24bc22000ec917ccbab4b5a
clam119 <christopher.lam119@gmail.com>
2022-10-27 12:46:28 +0100

    Add Marketplace, useState hook, UserContext function created with Context API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+// import Marketplace from './components/Marketplace';
+import Login from './components/Login';
+import { UserContext } from './components/context/UserContext';
+
 function App() {
+  const [username, setUsername] = useState('Not Logged In');
+
   return (
+  <UserContext.Provider value={{ username, setUsername }}>
     <div className="App">
-      <Header />
-      <Home/>
-      <Footer/>
+      <BrowserRouter>
+          <Header />
+            <Routes>
+              <Route path = "/" element={ <Home />} />
+              <Route path = "/login" element={ <Login />} />
+            </Routes>
+          <Footer/>
+     </BrowserRouter>
     </div>
+  </UserContext.Provider>
   );
 }
 
