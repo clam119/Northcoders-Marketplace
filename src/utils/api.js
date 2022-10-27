7c4a88467e56154e4e825e40ad1f43c6f1322482
clam119 <christopher.lam119@gmail.com>
2022-10-27 12:46:36 +0100

    initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -11,3 +11,25 @@ export const fetchAllItems = () => {
         return response.data;
     })
 }
+
+export const fetchItemById = (item_id) => {
+    return marketplaceApi.get(`/items/${item_id}`)
+    .then((response) => {
+        return response.data
+    })
+}
+
+export const deleteItemById = (item_id) => {
+    return marketplaceApi.delete(`/items/${item_id}`)
+    .then((response) => {
+        return response.data;
+    })
+}
+
+export const fetchAllUsers = () => {
+    return marketplaceApi.get('/users')
+    .then((response) => {
+        console.log(response.data.users)
+        return response.data.users;
+    })
+}
